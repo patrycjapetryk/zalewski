481a0fac9faabc2dea37afac217a031861d7ddc3
Patrycja Petryk <51820512+patrycjapetryk@users.noreply.github.com>
2024-04-16 19:49:10 +0200

    Add sticky menu on desktop

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -4,10 +4,15 @@ import Navigation from '../ui/Navigation';
 
 export default function Header() {
   return (
-    <header className="flex items-center justify-between">
-      <Logo />
-      <Navigation />
-      <ButtonLink>Kontakt</ButtonLink>
+    <header className="z-10 w-full lg:fixed lg:left-0 lg:top-0">
+      <div
+        className="m-auto flex w-full max-w-screen-3xl items-center justify-between px-5 py-6
+        sm:px-10 md:px-[5%] md:py-10 xl:px-[7%] 2xl:px-[10%]"
+      >
+        <Logo />
+        <Navigation />
+        <ButtonLink>Kontakt</ButtonLink>
+      </div>
     </header>
   );
 }
--- components/ui/LayoutWrapper.tsx
+++ components/ui/LayoutWrapper.tsx
@@ -4,7 +4,13 @@ export default function PageWrapper({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="m-auto w-full max-w-screen-3xl overflow-x-hidden px-5 py-6 sm:px-10 md:px-[5%] md:py-10 xl:px-[7%] 2xl:px-[10%]">
+    <div
+      className={`
+        m-auto w-full max-w-screen-3xl overflow-x-hidden
+        px-5 py-6
+        sm:px-10 md:px-[5%] md:py-10 lg:pt-28 xl:px-[7%] 2xl:px-[10%]
+      `}
+    >
       {children}
     </div>
   );
